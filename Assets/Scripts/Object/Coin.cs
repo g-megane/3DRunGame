037ff350b6a2287ac0948dc36a82856d54390650
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-10-31 20:55:29 +0900

    コインクラスの作成

---
+++ Assets/Scripts/Object/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コインの制御クラス
+/// </summary>
+public class Coin : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            gameObject.SetActive(false);
+        }
+    }
+}
