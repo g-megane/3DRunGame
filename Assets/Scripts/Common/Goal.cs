fff4d263fea47ec50a74c89d7a2fa8d10d019cdd
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-10-31 17:45:41 +0900

    ゴール用のクラスを作成

---
+++ Assets/Scripts/Common/Goal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ゴールオブジェクトの制御クラス
+/// </summary>
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // プレイヤーと接触した場合
+        if (other.tag == "Player") {
+            //TODO: ゴール時の処理
+            Debug.Log("Goal");
+        }
+    }
+}
