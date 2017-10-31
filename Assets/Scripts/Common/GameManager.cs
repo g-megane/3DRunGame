ce0dc1072c05a763af6f21bef08216e43da4185a
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-10-31 18:32:43 +0900

    ゲームマネージャークラスの作成

---
+++ Assets/Scripts/Common/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームマネージャークラス(シングルトン)
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// 唯一のインスタンス
+    /// </summary>
+    static GameManager instance;
+    public static GameManager Instance {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        // シングルトンの処理
+        if (Instance == null) {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// クリア時の処理
+    /// </summary>
+    public void gameCrear()
+    {
+
+    }
+
+    /// <summary>
+    /// ゲームオーバー時の処理
+    /// </summary>
+    public void gameOver()
+    {
+
+    }
+}
