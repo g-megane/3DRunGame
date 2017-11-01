b0329d525aad05d48887204e05f639ba545f4d39
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-01 17:44:25 +0900

    コインのランダム配置の修正

--- Assets/Scripts/Common/StageCreator.cs
+++ Assets/Scripts/Common/StageCreator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -35,6 +36,9 @@ public class StageCreator : MonoBehaviour
     {
         // ステージの生成
         createStage();
+
+        // ステージ生成ご自らを破棄
+        Destroy(gameObject);
     }
 
     /// <summary>
@@ -42,20 +46,29 @@ public class StageCreator : MonoBehaviour
     /// </summary>
     void createStage()
     {
-        //TODO: 関数に分ける
+        createFloor();
+
+        createCoin();
+
+        createGoal();
+    }
+
+    void createFloor()
+    {
         float xMoveValue = stageBlock[0].GetComponent<Renderer>().bounds.size.x; // 生成位置の移動量X
         //float yMoveValue = stageBlock[0].GetComponent<Renderer>().bounds.size.y;  // 生成位置の移動量Y
 
         // 最初の10個の床はスペースを空けない
         for (int i = 0; i < 10; ++i) {
-            var ranNum = Random.Range(0, stageBlock.Count);
-            var obj = Instantiate(stageBlock[ranNum]);
+            var ranNum = UnityEngine.Random.Range(0, stageBlock.Count);
+            var obj = Instantiate(stageBlock[ranNum]) as GameObject;
             obj.transform.position = createPos;
             createPos.Set(createPos.x + xMoveValue, 0.0f, 0.0f);
         }
 
-        for (int i = 0; createPos.x < 1050; ++i) {
-            var ranNum = Random.Range(0, stageBlock.Count + 1);
+        // 980mまではランダムに生成
+        for (int i = 0; createPos.x < 980; ++i) {
+            var ranNum = UnityEngine.Random.Range(0, stageBlock.Count + 1);
 
             // スペースを開ける
             if (ranNum == stageBlock.Count) {
@@ -63,7 +76,8 @@ public class StageCreator : MonoBehaviour
                 if (continualSpaceCount >= 2) {
                     continualSpaceCount = 0;
                     --i;
-                } 
+                    continue;
+                }
             }
             // オブジェクトの生成
             else {
@@ -73,12 +87,31 @@ public class StageCreator : MonoBehaviour
             createPos.Set(createPos.x + xMoveValue, 0.0f, 0.0f);
         }
 
+        // 最後の数mは床を配置
+        for (int i = 0; createPos.x < 1050; ++i) {
+            var ranNum = UnityEngine.Random.Range(0, stageBlock.Count);
+            var obj = Instantiate(stageBlock[ranNum]) as GameObject;
+            obj.transform.position = createPos;
+            createPos.Set(createPos.x + xMoveValue, 0.0f, 0.0f);
+        }
+    }
+
+    void createCoin()
+    {
+        int[] usedCoinPosX = new int[100];
+
         // コインの配置
         for (int i = 0; i < 100; ++i) {
             var coinObj = Instantiate(coin) as GameObject;
-            coin.transform.position = new Vector3(Random.Range(5, 990), Random.Range(2, 6), 0.0f);
+            var x = UnityEngine.Random.Range(5, 990);
+            var y = UnityEngine.Random.Range(2, 6);
+            coin.transform.position = new Vector3(x, y, 0.0f);
         }
+    }
 
+    void createGoal()
+    {
+        // ゴールの生成
         var goal = Instantiate(goalObject) as GameObject;
         goal.transform.position = new Vector3(1000.0f, 2.0f, 0.0f);
     }
