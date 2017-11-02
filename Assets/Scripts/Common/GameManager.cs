9201af991cb76d73ed0a412cc7825b4277c09adc
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-02 12:31:07 +0900

    コインの獲得数とゴールまでの距離を表す変数を追加

--- Assets/Scripts/Common/GameManager.cs
+++ Assets/Scripts/Common/GameManager.cs
@@ -15,6 +15,32 @@ public class GameManager : MonoBehaviour
         get { return instance; }
     }
 
+    /// <summary>
+    /// 獲得コイン数
+    /// </summary>
+    int coinCount = 0;
+    public int CoinCount {
+        get { return coinCount; }
+    }
+
+    /// <summary>
+    /// ゴールまでの距離
+    /// </summary>
+    float distanceToGoal = 0.0f;
+    public float DistanceToGoal {
+        get { return distanceToGoal; }
+    }
+
+    /// <summary>
+    /// プレイヤーの参照
+    /// </summary>
+    GameObject player;
+
+    /// <summary>
+    /// ゴールオブジェクトの参照
+    /// </summary>
+    GameObject goal;
+
     void Awake()
     {
         // シングルトンの処理
@@ -25,12 +51,26 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-
+        player = GameObject.FindWithTag("Player");
+        goal   = GameObject.FindWithTag("Goal");
     }
 
     void Update()
     {
+        checkDistanceToGoal();
+    }
 
+    void checkDistanceToGoal()
+    {
+        distanceToGoal = goal.transform.position.x - player.transform.position.x;
+    }
+
+    /// <summary>
+    /// コインを獲得した
+    /// </summary>
+    public void getCoin()
+    {
+        ++coinCount;
     }
 
     /// <summary>
