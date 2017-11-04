23552ed3619e61f39593da67a203f18668d69d6e
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-04 10:19:43 +0900

    SkyBoxを追加

---
+++ Assets/Scripts/UI/DistanceToGoalText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceToGoalText : MonoBehaviour
+{
+    Text distanceToGoal;
+
+    void Start()
+    {
+        distanceToGoal = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        distanceToGoal.text = string.Format("残り{0:0000}m", GameManager.Instance.DistanceToGoal);
+    }
+}
