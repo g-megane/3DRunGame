ca2ca1d5a19d2c21a97d0ce83d43aca2f5105641
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-05 02:19:56 +0900

    ゲームデータをまとめたScriptableObjectを作成

---
+++ Assets/Scripts/Common/GameData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ゲームデータをまとめたScriptableObject
+/// </summary>
+[CreateAssetMenu]
+public class GameData : ScriptableObject
+{
+    /// <summary>
+    /// ゴールまでの距離
+    /// </summary>
+    [SerializeField, Header("ゴールまでの距離"), Range(0, 1000)]
+    float goalDistance = 1000;
+    public float GoalDistance {
+        get { return goalDistance; }
+    }
+
+    public int coinCount = 0;
+
+    public float distanceToGoal = 0.0f;
+}
