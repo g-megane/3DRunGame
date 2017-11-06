d893874e28d6d5694b765f110fdabae1a895a38c
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-06 12:14:41 +0900

    MainシーンのUIを管理するクラスを作成
    ・１UIに１クラスを廃止

---
+++ Assets/Scripts/UI/MainSceneUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// メインシーンのUIを制御するクラス
+/// </summary>
+public class MainSceneUI : MonoBehaviour
+{
+    /// <summary>
+    /// ゲームデータを保有するScriptableObject
+    /// </summary>
+    [SerializeField, Header("GameDataのScriptableObject")]
+    GameData gameData;
+
+    /// <summary>
+    /// コインの取得数を表示するText
+    /// </summary>
+    [SerializeField, Header("コインの取得数を表示するテキスト")]
+    Text coinCount;
+
+    /// <summary>
+    /// ゴールまでの距離を表示するText
+    /// </summary>
+    [SerializeField, Header("ゴールまでの距離を表示するテキスト")]
+    Text distanceToGoal;
+
+    void Update()
+    {
+        updateDistanceToGoal();
+        updateCoinCount();
+    }
+
+    /// <summary>
+    /// コイン取得数のテキストの更新
+    /// </summary>
+    void updateCoinCount()
+    {
+        coinCount.text = string.Format("Coin : {0:000}", gameData.CoinCoint);
+    }
+
+    void updateDistanceToGoal()
+    {
+        distanceToGoal.text = string.Format("残り{0:0000}m", GameManager.Instance.DistanceToGoal);
+    }
+}
