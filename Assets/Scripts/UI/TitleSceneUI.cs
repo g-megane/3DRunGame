a396a6c594b5947d0293fd63c0848d15251f4b2d
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-09 17:27:24 +0900

    TitleシーンのUI制御クラスを作成

---
+++ Assets/Scripts/UI/TitleSceneUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// タイトルシーン用のUI制御クラス
+/// </summary>
+public class TitleSceneUI : MonoBehaviour
+{
+    public void onClickStart()
+    {
+        MySceneManager.changeScene("Main");
+    }
+}
