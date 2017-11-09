981a6f81b9ae2e0572b13d584204217886e4e622
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-09 13:48:58 +0900

    シーン遷移を制御するクラスを追加

---
+++ Assets/Scripts/Common/MySceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// シーン遷移の管理クラス
+/// </summary>
+public class MySceneManager : MonoBehaviour
+{
+    public static void changeScene(string sceneName)
+    {
+        if (FadeImage.IsDuringFade) { return; }
+        FadeImage.fadeOut(sceneName);
+    }
+}
