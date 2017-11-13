d13eff8b1476611f6ae67f2c9e8e0cf7c9b74b72
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-13 13:39:52 +0900

    タイトルのUIの調整

--- Assets/Scripts/UI/TitleSceneUI.cs
+++ Assets/Scripts/UI/TitleSceneUI.cs
@@ -6,6 +6,13 @@
 public class TitleSceneUI : MonoBehaviour
 {
 
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        Screen.SetResolution(1600, 900, false, 60);
+    }
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R)) {
@@ -13,8 +20,19 @@ public class TitleSceneUI : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// スタートボタンが押された時の動作
+    /// </summary>
     public void onClickStart()
     {
         MySceneManager.changeScene("Main");
+    }
+
+    /// <summary>
+    /// 操作説明ボタンが押された時の動作
+    /// </summary>
+    public void onClickOperating()
+    {
+        MySceneManager.changeScene("Operating");
     }
 }
