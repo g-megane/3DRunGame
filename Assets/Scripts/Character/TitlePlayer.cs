e8f4649b45287f5f88889ab1342f5dd2ad860200
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-11-13 12:39:04 +0900

    タイトル用のキャラクターモデルの制御クラスを作成

---
+++ Assets/Scripts/Character/TitlePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitlePlayer : MonoBehaviour
+{    
+    /// <summary>
+    /// Animatorの参照
+    /// </summary>
+    Animator animator;
+
+    /// <summary>
+    /// スタートが押されたか？
+    /// </summary>
+    bool isStart = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!isStart) { return; }
+        transform.localPosition += (Vector3.back + Vector3.right) * 0.1f;
+    }
+
+    public void onClickStart()
+    {
+        isStart = true;
+        animator.SetBool("IsRun", true);
+    }
+}
